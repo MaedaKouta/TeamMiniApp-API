746ea032835f8183dad27ef6e8717c2fe8a6f9be
Kota <maedakoutaa@gmail.com>
2022-04-06 16:45:12 +0900

    initial commit

---
+++ TeamMiniApp-API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamMiniApp-API
+//
+//  Created by 前田航汰 on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
